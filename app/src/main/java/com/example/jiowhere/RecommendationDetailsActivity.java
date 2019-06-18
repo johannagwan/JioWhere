9723dfd9c33030f58c99aadae51716bdd4ffc312
gracegrape <e0324121@u.nus.edu>
2019-06-18 15:37:59 +0800

    Data get passed now for 1) Activity Name 2) Location (Nearest MRT) 3) Image 4) Time Period
    Will include the other data when firebase is working :')

--- app/src/main/java/com/example/jiowhere/RecommendationDetailsActivity.java
+++ app/src/main/java/com/example/jiowhere/RecommendationDetailsActivity.java
@@ -25,6 +25,10 @@ public class RecommendationDetailsActivity extends AppCompatActivity implements 
     //This is the full recommendation -> the detailed recommendation
 
     ListView mListView;
+    TextView name;
+    TextView location;
+    TextView time;
+    ImageView image;
 
     int[] images = {R.drawable.sentosa, R.drawable.two, R.drawable.three, R.drawable.four};
     String[] reviews = {"first", "2nd", "sdddddddddddddddddddddddddddddddddddddddddddddddddddddddd", "nxw"};
@@ -36,9 +40,33 @@ public class RecommendationDetailsActivity extends AppCompatActivity implements 
         setContentView(R.layout.recommendation_layout);
 
         mListView = (ListView) findViewById(R.id.reviewsList);
-
         CustomAdaptor customAdaptor = new CustomAdaptor();
         mListView.setAdapter(customAdaptor);
+
+        //Passing data inside
+        name = (TextView) findViewById(R.id.activityNameTitle);
+        location = (TextView) findViewById(R.id.mrtName);
+        time = (TextView) findViewById(R.id.timePeriod);
+        image = (ImageView) findViewById(R.id.imageView);
+
+
+        // Receiving value into activity using intent.
+        String activityName = getIntent().getStringExtra("name");
+        String activityLocation = getIntent().getStringExtra("location");
+        String activityTime = getIntent().getStringExtra("time");
+        //int activityImage = getIntent().getIntExtra();
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            int picture = bundle.getInt("picture");
+            image.setImageResource(picture);
+        }
+
+
+
+        // Setting up received value into EditText.
+        name.setText(activityName);
+        location.setText(activityLocation);
+        time.setText(activityTime);
 
     }
 
