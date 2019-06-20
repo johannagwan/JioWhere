1bc6152a2edc333eeadfc1152d434ae012fe660f
gracegrape <e0324121@u.nus.edu>
2019-06-20 13:49:06 +0800

    Added leave a review button

---
+++ app/src/main/java/com/example/jiowhere/ReviewActivity.java
@@ -0,0 +1,13 @@
+package com.example.jiowhere;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.leave_a_review_layout);
+    }
+}
